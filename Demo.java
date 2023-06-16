b225c4e6663958c860a7e5292618ad3f1eba9847
Syed Minnatullah - Quadri <88645248+s-m-quadri@users.noreply.github.com>
2023-06-16 12:42:45 +0530

    Fprmat Demo.java

--- Demo.java
+++ Demo.java
@@ -1,12 +1,11 @@
 class Demo {
-public static void main(String [] args)
-{
-System.out.println("hello all");
-System.out.println(args[0]);
-System.out.println(args[1]);
-
-System.out.println("hello all");
-System.out.println(args[0]);
-System.out.println(args[1]);
-}
+  public static void main(String [] args) {
+    System.out.println("Assalam.. all");
+    System.out.println(args[0]);
+    System.out.println(args[1]);
+    
+    System.out.println("hello all");
+    System.out.println(args[0]);
+    System.out.println(args[1]);
+    }
 }
